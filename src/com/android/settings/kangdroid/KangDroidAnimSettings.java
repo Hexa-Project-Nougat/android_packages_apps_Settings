9e6f30aeab67fde8cdfed3c5d35ec2a751dbeae9
GwonHyeok <kh4975@naver.com>
2014-03-06 11:31:43 +0100

    Settings - XuiMod: Toast Animations [2/2]
    Change-Id: Id6ef51286fcaa88e60ace6daae5a14301fac2aae

---
+++ src/com/android/settings/kangdroid/KangDroidAnimSettings.java
@@ -0,0 +1,127 @@
+/*
+* Copyright (C) 2015 Benzo Rom
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.settings.kangdroid;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.CheckBoxPreference;
+
+import android.os.UserHandle;
+import android.view.Display;
+import android.view.IWindowManager;
+import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
+import android.view.WindowManagerImpl;
+import android.widget.Toast;
+import com.android.internal.view.RotationPolicy;
+import com.android.settings.DropDownPreference.Callback;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.Dialog;
+import android.app.IActivityManager;
+import android.app.ProgressDialog;
+import android.app.UiModeManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.support.v7.ListPreference;
+import android.support.v7.Preference;
+import android.support.v7.PreferenceManager;
+import android.support.v7.Preference.OnPreferenceClickListener;
+import android.support.v7.PreferenceCategory;
+import android.support.v7.PreferenceManager;
+import android.support.v7.PreferenceScreen;
+import android.support.v14.SwitchPreference;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.android.settings.Utils;
+
+public class KangDroidAnimSettings extends SettingsPreferenceFragment
+            implements OnPreferenceChangeListener  {
+		
+	private static final String TAG = "KangDroidAnimSettings";
+	private static final String KEY_TOAST_ANIMATION = "toast_animation";
+	
+	private Context mContext;
+	private ListPreference mToastAnimation;
+	
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.kangdroid_anim_settings);
+		PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mContext = getActivity().getApplicationContext();
+
+        // Toast Animations
+        mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
+        mToastAnimation.setSummary(mToastAnimation.getEntry());
+        int CurrentToastAnimation = Settings.System.getInt(resolver,
+                Settings.System.TOAST_ANIMATION, 1);
+        mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
+        mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
+        mToastAnimation.setOnPreferenceChangeListener(this);
+    }
+	
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+	
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        if (preference == mToastAnimation) {
+            int index = mToastAnimation.findIndexOfValue((String) objValue);
+            Settings.System.putString(getContentResolver(), Settings.System.TOAST_ANIMATION, (String) objValue);
+            mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+            Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+   }
+
+	
+   @Override
+   protected int getMetricsCategory() {
+       return MetricsEvent.APPLICATION;
+   }
+}
