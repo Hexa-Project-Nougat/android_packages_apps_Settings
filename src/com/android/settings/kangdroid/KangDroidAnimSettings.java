3bcf8cb0bb09229af305cff319f94e6f037b093a
KangDroid <hyunwoo286@gmail.com>
2016-09-18 15:28:19 +0900

    Fix error
    Revert "Fix error"
    
    This reverts commit 09cf7460f29ae66aaa00bb178b2b744a998728f7.

--- src/com/android/settings/kangdroid/KangDroidAnimSettings.java
+++ src/com/android/settings/kangdroid/KangDroidAnimSettings.java
@@ -17,62 +17,21 @@ package com.android.settings.kangdroid;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.support.v7.preference.CheckBoxPreference;
-
-import android.os.UserHandle;
-import android.view.Display;
-import android.view.IWindowManager;
-import android.view.WindowManager;
-import android.view.WindowManagerGlobal;
-import android.view.WindowManagerImpl;
-import android.widget.Toast;
-import com.android.internal.view.RotationPolicy;
-import com.android.settings.DropDownPreference.Callback;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.search.Indexable;
-
 import android.app.Activity;
-import android.app.ActivityManagerNative;
-import android.app.Dialog;
-import android.app.IActivityManager;
-import android.app.ProgressDialog;
-import android.app.UiModeManager;
-import android.app.admin.DevicePolicyManager;
+import android.os.Bundle;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.database.ContentObserver;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceManager;
-import android.support.v7.preference.Preference.OnPreferenceClickListener;
-import android.support.v7.preference.PreferenceCategory;
-import android.support.v7.preference.PreferenceManager;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v14.preference.SwitchPreference;
-import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import android.text.TextUtils;
-import android.util.DisplayMetrics;
-import android.util.Log;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-import com.android.settings.Utils;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
 
 public class KangDroidAnimSettings extends SettingsPreferenceFragment
             implements OnPreferenceChangeListener  {
@@ -113,14 +72,14 @@ public class KangDroidAnimSettings extends SettingsPreferenceFragment
 		
         // List view animation
         mListViewAnimation = (ListPreference) findPreference(KEY_LISTVIEW_ANIMATION);
-        int listviewanimation = Settings.System.getInt(getContentResolver(),
+        int listviewanimation = Settings.System.getInt(resolver,
                 Settings.System.LISTVIEW_ANIMATION, 0);
         mListViewAnimation.setValue(String.valueOf(listviewanimation));
         mListViewAnimation.setSummary(mListViewAnimation.getEntry());
         mListViewAnimation.setOnPreferenceChangeListener(this);
 
         mListViewInterpolator = (ListPreference) findPreference(KEY_LISTVIEW_INTERPOLATOR);
-        int listviewinterpolator = Settings.System.getInt(getContentResolver(),
+        int listviewinterpolator = Settings.System.getInt(resolver,
                 Settings.System.LISTVIEW_INTERPOLATOR, 0);
         mListViewInterpolator.setValue(String.valueOf(listviewinterpolator));
         mListViewInterpolator.setSummary(mListViewInterpolator.getEntry());
@@ -141,9 +100,10 @@ public class KangDroidAnimSettings extends SettingsPreferenceFragment
 	
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
+		ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mToastAnimation) {
             int index = mToastAnimation.findIndexOfValue((String) objValue);
-            Settings.System.putString(getContentResolver(), Settings.System.TOAST_ANIMATION, (String) objValue);
+            Settings.System.putString(resolver, Settings.System.TOAST_ANIMATION, (String) objValue);
             mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
             Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
             return true;
@@ -151,7 +111,7 @@ public class KangDroidAnimSettings extends SettingsPreferenceFragment
         if (KEY_LISTVIEW_ANIMATION.equals(key)) {
             int value = Integer.parseInt((String) objValue);
             int index = mListViewAnimation.findIndexOfValue((String) objValue);
-            Settings.System.putInt(getContentResolver(),
+            Settings.System.putInt(resolver,
                     Settings.System.LISTVIEW_ANIMATION,
                     value);
             mListViewAnimation.setSummary(mListViewAnimation.getEntries()[index]);
@@ -160,7 +120,7 @@ public class KangDroidAnimSettings extends SettingsPreferenceFragment
         if (KEY_LISTVIEW_INTERPOLATOR.equals(key)) {
             int value = Integer.parseInt((String) objValue);
             int index = mListViewInterpolator.findIndexOfValue((String) objValue);
-            Settings.System.putInt(getContentResolver(),
+            Settings.System.putInt(resolver,
                     Settings.System.LISTVIEW_INTERPOLATOR,
                     value);
             mListViewInterpolator.setSummary(mListViewInterpolator.getEntries()[index]);
