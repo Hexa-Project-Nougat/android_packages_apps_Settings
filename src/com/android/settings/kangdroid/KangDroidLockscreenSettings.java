296fb8e84319808cc5c1e885054a9a922593cad0
KangDroid <hyunwoo286@gmail.com>
2016-10-06 19:53:58 +0900

    Add Lockscreen Fragment

---
+++ src/com/android/settings/kangdroid/KangDroidLockscreenSettings.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2014 The KangDroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.kangdroid;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.provider.SearchIndexableResource;
+import com.android.settings.kangdroid.SeekBarPreference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KangDroidLockscreenSettings extends SettingsPreferenceFragment implements Indexable, Preference.OnPreferenceChangeListener {
+	
+	private static final String PREF_ON_THE_GO_ALPHA = "on_the_go_alpha";
+	
+	private SeekBarPreference mOnTheGoAlphaPref;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.kangdroid_lockscreen_settings);
+        mOnTheGoAlphaPref = (SeekBarPreference) findPreference(PREF_ON_THE_GO_ALPHA);
+        mOnTheGoAlphaPref.setValue(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.ON_THE_GO_ALPHA, 50));
+        mOnTheGoAlphaPref.setOnPreferenceChangeListener(this);
+    }
+	
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+	
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mOnTheGoAlphaPref) {
+            float val = Float.parseFloat((String) objValue);
+            Settings.System.putFloat(getActivity().getContentResolver(), Settings.System.ON_THE_GO_ALPHA,
+                    val / 100);
+            return true;
+        }
+        return false;
+    }
+	
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
+}
