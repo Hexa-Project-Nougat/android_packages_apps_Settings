6772aebfc9c4d61d06bf088e347854edd032ae97
xyyx <xyyx@mail.ru>
2016-10-13 09:36:35 +0200

    Custom small QS tiles [2/2]
    Change-Id: Ia0eb8b6a679b627d11d3df1f798f92f21293bda5

--- src/com/android/settings/kangdroid/KangDroidQuickSettings.java
+++ src/com/android/settings/kangdroid/KangDroidQuickSettings.java
@@ -59,6 +59,7 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
     private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
     private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
     private static final String PREF_COLUMNS = "qs_columns";
+	private static final String KEY_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
 
     private ListPreference mTileAnimationStyle;
     private ListPreference mTileAnimationDuration;
@@ -66,6 +67,7 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
     private ListPreference mRowsPortrait;
     private ListPreference mRowsLandscape;
     private ListPreference mQsColumns;
+	private ListPreference mSysuiQqsCount;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -122,6 +124,13 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
         mQsColumns.setValue(String.valueOf(columnsQs));
         mQsColumns.setSummary(mQsColumns.getEntry());
         mQsColumns.setOnPreferenceChangeListener(this);
+		
+        mSysuiQqsCount = (ListPreference) findPreference(KEY_SYSUI_QQS_COUNT);
+        int SysuiQqsCount = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.QQS_COUNT, 5);
+        mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
+        mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
+        mSysuiQqsCount.setOnPreferenceChangeListener(this);
 
     }
 	
@@ -173,6 +182,13 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.QS_COLUMNS, intValue);
             preference.setSummary(mQsColumns.getEntries()[index]);
+            return true;
+        } else if (preference == mSysuiQqsCount) {
+            String SysuiQqsCount = (String) objValue;
+            int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+            int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
+            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
             return true;
         }
         return false;
