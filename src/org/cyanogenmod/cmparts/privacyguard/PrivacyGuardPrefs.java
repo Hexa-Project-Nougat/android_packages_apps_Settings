c0796541b35fd0602ed0550333455f1f675049b3
KangDroid <hyunwoo286@gmail.com>
2016-10-21 22:30:32 +0900

    Fix FC

--- src/org/cyanogenmod/cmparts/privacyguard/PrivacyGuardPrefs.java
+++ src/org/cyanogenmod/cmparts/privacyguard/PrivacyGuardPrefs.java
@@ -19,10 +19,10 @@ package org.cyanogenmod.cmparts.privacyguard;
 
 import android.os.Bundle;
 import android.provider.Settings;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceFragment;
-import android.preference.SwitchPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -47,8 +47,7 @@ public class PrivacyGuardPrefs extends PreferenceFragment implements
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 
         addPreferencesFromResource(R.xml.privacy_guard_prefs);
 
@@ -64,7 +63,7 @@ public class PrivacyGuardPrefs extends PreferenceFragment implements
     public View onCreateView(LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         final View view = super.onCreateView(inflater, container, savedInstanceState);
-        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        final ViewGroup list = (ViewGroup) getListView().getParent();
         // our container already takes care of the padding
         int paddingTop = list.getPaddingTop();
         int paddingBottom = list.getPaddingBottom();
