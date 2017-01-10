283b4f64003fd877f576cf318c9cdf49a091f1ab
maxwen <max.weninger@gmail.com>
2017-01-10 14:43:50 +0530

    Header Image Packs and Shadows {SQUASHED}[2/2]
    Change-Id: I82b2bb36b746ce304fd7113491e52966f9b9a6fe
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/kangdroid/KangDroidQuickSettings.java
+++ src/com/android/settings/kangdroid/KangDroidQuickSettings.java
@@ -16,6 +16,13 @@
 
 package com.android.settings.kangdroid;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.app.ActivityManagerNative;
 import android.content.Context;
 import android.content.ContentResolver;
@@ -30,6 +37,10 @@ import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.provider.Settings;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.WindowManagerGlobal;
@@ -45,11 +56,16 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
+import com.android.settings.kangdroid.KangDroidSeekBarPreference;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class KangDroidQuickSettings extends SettingsPreferenceFragment implements Indexable, Preference.OnPreferenceChangeListener {
 
@@ -60,6 +76,13 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
     private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
     private static final String PREF_COLUMNS = "qs_columns";
 	private static final String KEY_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+    private static final String CUSTOM_HEADER_IMAGE = "status_bar_custom_header";
+    private static final String DAYLIGHT_HEADER_PACK = "daylight_header_pack";
+    private static final String DEFAULT_HEADER_PACKAGE = "com.android.systemui";
+    private static final String CUSTOM_HEADER_IMAGE_SHADOW = "status_bar_custom_header_shadow";
+
+    private ListPreference mDaylightHeaderPack;
+    private SeekBarPreference mHeaderShadow;
 
     private ListPreference mTileAnimationStyle;
     private ListPreference mTileAnimationDuration;
@@ -131,6 +154,37 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
         mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
         mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
         mSysuiQqsCount.setOnPreferenceChangeListener(this);
+		
+             String settingHeaderPackage = Settings.System.getString(resolver,
+                     Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK);
+             if (settingHeaderPackage == null) {
+                 settingHeaderPackage = DEFAULT_HEADER_PACKAGE;
+             }
+             mDaylightHeaderPack = (ListPreference) findPreference(DAYLIGHT_HEADER_PACK);
+ 
+             List<String> entries = new ArrayList<String>();
+             List<String> values = new ArrayList<String>();
+             getAvailableHeaderPacks(entries, values);
+             mDaylightHeaderPack.setEntries(entries.toArray(new String[entries.size()]));
+             mDaylightHeaderPack.setEntryValues(values.toArray(new String[values.size()]));
+ 
+             int valueIndex = mDaylightHeaderPack.findIndexOfValue(settingHeaderPackage);
+             if (valueIndex == -1) {
+                 // no longer found
+                 settingHeaderPackage = DEFAULT_HEADER_PACKAGE;
+                 Settings.System.putString(resolver,
+                         Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, settingHeaderPackage);
+                 valueIndex = mDaylightHeaderPack.findIndexOfValue(settingHeaderPackage);
+             }
+             mDaylightHeaderPack.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+             mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntry());
+             mDaylightHeaderPack.setOnPreferenceChangeListener(this);
+ 
+             mHeaderShadow = (KangDroidSeekBarPreference) findPreference(CUSTOM_HEADER_IMAGE_SHADOW);
+             final int headerShadow = Settings.System.getInt(resolver,
+                     Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, 80);
+             mHeaderShadow.setValue((int)(((double) headerShadow / 255) * 100));
+             mHeaderShadow.setOnPreferenceChangeListener(this);
 
     }
 	
@@ -141,6 +195,7 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
 	
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+		ContentResolver resolver = getActivity().getContentResolver();
 		int intValue;
 		int index;
         if (preference == mTileAnimationStyle) {
@@ -190,6 +245,19 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
             int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
             mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
             return true;
+             } else if (preference == mDaylightHeaderPack) {
+                  String value = (String) newValue;
+                  Settings.System.putString(resolver,
+                          Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, value);
+                  int valueIndex = mDaylightHeaderPack.findIndexOfValue(value);
+                  mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntries()[valueIndex]);
+                  return true;
+              } else if (preference == mHeaderShadow) {
+                  Integer headerShadow = (Integer) newValue;
+                  int realHeaderValue = (int) (((double) headerShadow / 100) * 255);
+                  Settings.System.putInt(resolver,
+                          Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, realHeaderValue);
+                  return true;
         }
         return false;
     }
@@ -223,6 +291,40 @@ public class KangDroidQuickSettings extends SettingsPreferenceFragment implement
             }
         }
     }
+	
+          private void getAvailableHeaderPacks(List<String> entries, List<String> values) {
+              Intent i = new Intent();
+              PackageManager packageManager = getActivity().getPackageManager();
+              i.setAction("org.omnirom.DaylightHeaderPack");
+              for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+                  String packageName = r.activityInfo.packageName;
+                  if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
+                      values.add(0, packageName);
+                  } else {
+                      values.add(packageName);
+                  }
+                  String label = r.activityInfo.loadLabel(getActivity().getPackageManager()).toString();
+                  if (label == null) {
+                      label = r.activityInfo.packageName;
+                  }
+                  if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
+                      entries.add(0, label);
+                  } else {
+                      entries.add(label);
+                  }
+              }
+              i.setAction("org.omnirom.DaylightHeaderPack1");
+              for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+                  String packageName = r.activityInfo.packageName;
+                  values.add(packageName  + "/"  + r.activityInfo.name);
+  
+                  String label = r.activityInfo.loadLabel(getActivity().getPackageManager()).toString();
+                  if (label == null) {
+                      label = packageName;
+                  }
+                  entries.add(label);
+              }
+          }
 	
     @Override
     protected int getMetricsCategory() {
