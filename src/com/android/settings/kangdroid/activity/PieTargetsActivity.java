afc93b2ac4efbaeaf0237faedd42252fdcbfcb1c
KangDroid <hyunwoo286@gmail.com>
2017-01-12 20:40:49 +0900

    Testing NEW TabLayout
    Change-Id: Id103f59ffbd9e1c4a9cae78fe388a3a47fba4834

---
+++ src/com/android/settings/kangdroid/activity/PieTargetsActivity.java
@@ -0,0 +1,18 @@
+package com.android.settings.kangdroid.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.crdroid.settings.fragments.PieTargets;
+
+public class PieTargetsActivity extends Activity {
+
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  // TODO Auto-generated method stub
+  super.onCreate(savedInstanceState);
+  
+  getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PieTargets()).commit();
+ }
+
+}
