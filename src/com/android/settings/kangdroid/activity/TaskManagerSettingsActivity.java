0cc82acd34b7f52737e0881224c3a2f91cb2c0c3
Varun Date <date.varun123@gmail.com>
2017-01-16 13:49:17 +0530

    Settings:Task Manager Customizations [2/2]
    Change-Id: Iacbeb102fcaff987a13256088a5442477b87b696
    Signed-off-by: Varun Date <date.varun123@gmail.com>

---
+++ src/com/android/settings/kangdroid/activity/TaskManagerSettingsActivity.java
@@ -0,0 +1,18 @@
+package com.android.settings.kangdroid.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.android.settings.rr.TaskManagerSettings;
+
+public class TaskManagerSettingsActivity extends Activity {
+
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  // TODO Auto-generated method stub
+  super.onCreate(savedInstanceState);
+  
+  getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new TaskManagerSettings()).commit();
+ }
+
+}
