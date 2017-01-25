b2280b7d619b38a46e0b0048ea188ecaf2c6bc42
KangDroid <hyunwoo286@gmail.com>
2017-01-25 14:57:58 +0900

    Remove preference which are  not needed anymore
    Change-Id: If542b9e47eeeff4abe288388961ef695cd3ba1ea

--- src/com/android/settings/kangdroid/KangDroidNavBarSettings.java
+++ src/com/android/settings/kangdroid/KangDroidNavBarSettings.java
@@ -62,9 +62,6 @@ public class KangDroidNavBarSettings extends SettingsPreferenceFragment implemen
     private static final String TAG = "SystemSettings";
 
     private static final String KEY_NAVIGATION_BAR_LEFT = "navigation_bar_left";
-    private static final String KEY_NAVIGATION_HOME_LONG_PRESS = "navigation_home_long_press";
-    private static final String KEY_NAVIGATION_HOME_DOUBLE_TAP = "navigation_home_double_tap";
-    private static final String KEY_NAVIGATION_RECENTS_LONG_PRESS = "navigation_recents_long_press";
     private static final String CATEGORY_NAVBAR = "navigation_bar_category";
 	private static final String NAVBAR_VISIBILITY = "navbar_visibility";
     private static final String KEY_NAVBAR_MODE = "navbar_mode";
@@ -80,9 +77,6 @@ public class KangDroidNavBarSettings extends SettingsPreferenceFragment implemen
 
     private SwitchPreference mDisableNavigationKeys;
     private SwitchPreference mNavigationBarLeftPref;
-    private ListPreference mNavigationHomeLongPressAction;
-    private ListPreference mNavigationHomeDoubleTapAction;
-    private ListPreference mNavigationRecentsLongPressAction;
 	
     private SwitchPreference mNavbarVisibility;
     private ListPreference mNavbarMode;
@@ -97,30 +91,6 @@ public class KangDroidNavBarSettings extends SettingsPreferenceFragment implemen
 	public static final int KEY_MASK_HOME = 0x01;
 
     private Handler mHandler;
-	
-    private enum Action {
-        NOTHING,
-        MENU,
-        APP_SWITCH,
-        SEARCH,
-        VOICE_SEARCH,
-        IN_APP_SEARCH,
-        LAUNCH_CAMERA,
-        SLEEP,
-        LAST_APP,
-        SPLIT_SCREEN;
-
-        public static Action fromIntSafe(int id) {
-            if (id < NOTHING.ordinal() || id > Action.values().length) {
-                return NOTHING;
-            }
-            return Action.values()[id];
-        }
-
-        public static Action fromSettings(ContentResolver cr, String setting, Action def) {
-            return fromIntSafe(CMSettings.System.getInt(cr, setting, def.ordinal()));
-        }
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -143,32 +113,6 @@ public class KangDroidNavBarSettings extends SettingsPreferenceFragment implemen
         mNavigationBarLeftPref = (SwitchPreference) findPreference(KEY_NAVIGATION_BAR_LEFT);
 		
 		mNavbarDynamic = (SwitchPreference) findPreference(NAVBAR_DYNAMIC);
-
-        Action defaultHomeLongPressAction = Action.fromIntSafe(res.getInteger(
-                com.android.internal.R.integer.config_longPressOnHomeBehavior));
-        Action defaultHomeDoubleTapAction = Action.fromIntSafe(res.getInteger(
-                com.android.internal.R.integer.config_doubleTapOnHomeBehavior));
-        Action homeLongPressAction = Action.fromSettings(resolver,
-                CMSettings.System.KEY_HOME_LONG_PRESS_ACTION,
-                defaultHomeLongPressAction);
-        Action homeDoubleTapAction = Action.fromSettings(resolver,
-                CMSettings.System.KEY_HOME_DOUBLE_TAP_ACTION,
-                defaultHomeDoubleTapAction);
-
-        // Navigation bar home long press
-        mNavigationHomeLongPressAction = initActionList(KEY_NAVIGATION_HOME_LONG_PRESS,
-                homeLongPressAction);
-
-        // Navigation bar home double tap
-        mNavigationHomeDoubleTapAction = initActionList(KEY_NAVIGATION_HOME_DOUBLE_TAP,
-                homeDoubleTapAction);
-
-        // Navigation bar recents long press activity needs custom setup
-        mNavigationRecentsLongPressAction =
-                initRecentsLongPressAction(KEY_NAVIGATION_RECENTS_LONG_PRESS);
-
-        final CMHardwareManager hardware = CMHardwareManager.getInstance(getActivity());
-
 		
         mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
         mNavbarMode = (ListPreference) findPreference(KEY_NAVBAR_MODE);
@@ -220,124 +164,10 @@ public class KangDroidNavBarSettings extends SettingsPreferenceFragment implemen
         super.onResume();
     }
 
-    private ListPreference initActionList(String key, Action value) {
-        return initActionList(key, value.ordinal());
-    }
-
-    private ListPreference initActionList(String key, int value) {
-        ListPreference list = (ListPreference) getPreferenceScreen().findPreference(key);
-        if (list == null) return null;
-        list.setValue(Integer.toString(value));
-        list.setSummary(list.getEntry());
-        list.setOnPreferenceChangeListener(this);
-        return list;
-    }
-
-    private ListPreference initRecentsLongPressAction(String key) {
-        ListPreference list = (ListPreference) getPreferenceScreen().findPreference(key);
-        list.setOnPreferenceChangeListener(this);
-
-        // Read the componentName from Settings.Secure, this is the user's prefered setting
-        String componentString = CMSettings.Secure.getString(getContentResolver(),
-                CMSettings.Secure.RECENTS_LONG_PRESS_ACTIVITY);
-        ComponentName targetComponent = null;
-        if (componentString == null) {
-            list.setSummary(getString(R.string.hardware_keys_action_split_screen));
-        } else {
-            targetComponent = ComponentName.unflattenFromString(componentString);
-        }
-
-        // Dyanamically generate the list array,
-        // query PackageManager for all Activites that are registered for ACTION_RECENTS_LONG_PRESS
-        PackageManager pm = getPackageManager();
-        Intent intent = new Intent(cyanogenmod.content.Intent.ACTION_RECENTS_LONG_PRESS);
-        List<ResolveInfo> recentsActivities = pm.queryIntentActivities(intent,
-                PackageManager.MATCH_DEFAULT_ONLY);
-        if (recentsActivities.size() == 0) {
-            // No entries available, disable
-            list.setSummary(getString(R.string.hardware_keys_action_split_screen));
-            CMSettings.Secure.putString(getContentResolver(),
-                    CMSettings.Secure.RECENTS_LONG_PRESS_ACTIVITY, null);
-            list.setEnabled(false);
-            return list;
-        }
-
-        CharSequence[] entries = new CharSequence[recentsActivities.size() + 1];
-        CharSequence[] values = new CharSequence[recentsActivities.size() + 1];
-        // First entry is always default last app
-        entries[0] = getString(R.string.hardware_keys_action_split_screen);
-        values[0] = "";
-        list.setValue(values[0].toString());
-        int i = 1;
-        for (ResolveInfo info : recentsActivities) {
-            try {
-                // Use pm.getApplicationInfo for the label,
-                // we cannot rely on ResolveInfo that comes back from queryIntentActivities.
-                entries[i] = pm.getApplicationInfo(info.activityInfo.packageName, 0).loadLabel(pm);
-            } catch (PackageManager.NameNotFoundException e) {
-                Log.e(TAG, "Error package not found: " + info.activityInfo.packageName, e);
-                // Fallback to package name
-                entries[i] = info.activityInfo.packageName;
-            }
-
-            // Set the value to the ComponentName that will handle this intent
-            ComponentName entryComponent = new ComponentName(info.activityInfo.packageName,
-                    info.activityInfo.name);
-            values[i] = entryComponent.flattenToString();
-            if (targetComponent != null) {
-                if (entryComponent.equals(targetComponent)) {
-                    // Update the selected value and the preference summary
-                    list.setSummary(entries[i]);
-                    list.setValue(values[i].toString());
-                }
-            }
-            i++;
-        }
-        list.setEntries(entries);
-        list.setEntryValues(values);
-        return list;
-    }
-
-    private void handleActionListChange(ListPreference pref, Object newValue, String setting) {
-        String value = (String) newValue;
-        int index = pref.findIndexOfValue(value);
-        pref.setSummary(pref.getEntries()[index]);
-        CMSettings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
-    }
-
-    private void handleSystemActionListChange(ListPreference pref, Object newValue, String setting) {
-        String value = (String) newValue;
-        int index = pref.findIndexOfValue(value);
-        pref.setSummary(pref.getEntries()[index]);
-        Settings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
-    }
-
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
 		final ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mNavigationRecentsLongPressAction) {
-            // RecentsLongPressAction is handled differently because it intentionally uses
-            // Settings.System
-            String putString = (String) newValue;
-            int index = mNavigationRecentsLongPressAction.findIndexOfValue(putString);
-            CharSequence summary = mNavigationRecentsLongPressAction.getEntries()[index];
-            // Update the summary
-            mNavigationRecentsLongPressAction.setSummary(summary);
-            if (putString.length() == 0) {
-                putString = null;
-            }
-            CMSettings.Secure.putString(getContentResolver(),
-                    CMSettings.Secure.RECENTS_LONG_PRESS_ACTIVITY, putString);
-            return true;
-		} else if (preference == mNavigationHomeLongPressAction) {
-            handleActionListChange((ListPreference) preference, newValue,
-                    CMSettings.System.KEY_HOME_LONG_PRESS_ACTION);
-            return true;
-		} else if (preference == mNavigationHomeDoubleTapAction) {
-		            handleActionListChange((ListPreference) preference, newValue,
-		                    CMSettings.System.KEY_HOME_DOUBLE_TAP_ACTION);
-		            return true;
-		} else if (preference == mNavbarMode) {
+        if (preference == mNavbarMode) {
 	            int mode = Integer.parseInt(((String) newValue).toString());
 	            Settings.Secure.putInt(getContentResolver(),
 	                    Settings.Secure.NAVIGATION_BAR_MODE, mode);
