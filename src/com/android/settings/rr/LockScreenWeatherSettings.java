1bc1b556b533a6c4eaf95a2d2c903cccda466295
Varun Date <date.varun123@gmail.com>
2017-04-13 04:15:50 +0530

    Settings: Refactor Keyguard Weather [2/2]
    --Point to Omnijaws for weather settings
    --Remove obselete icon pack settings
    
    Change-Id: I0dda70cd24a70250da529d7bc99995b0b19dbdf1
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/LockScreenWeatherSettings.java
+++ src/com/android/settings/rr/LockScreenWeatherSettings.java
@@ -21,19 +21,30 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
+import android.database.Cursor;
+import android.net.Uri;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceCategory;
 import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import android.provider.SearchIndexableResource;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -41,6 +52,17 @@ import com.android.settings.search.Indexable;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import android.util.Log;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.android.internal.util.rr.PackageUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -52,21 +74,25 @@ import net.margaritov.preference.colorpicker.ColorPickerPreference;
 public class LockScreenWeatherSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String PREF_CONDITION_ICON =
-            "weather_condition_icon";
     private static final String PREF_HIDE_WEATHER =
             "weather_hide_panel";
     private static final String PREF_NUMBER_OF_NOTIFICATIONS =
             "weather_number_of_notifications";
 
-    private static final int MONOCHROME_ICON = 0;
+	private static final String CATEGORY_WEATHER = "weather_category";
+	private static final String WEATHER_ICON_PACK = "weather_icon_pack";
+	private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws";
+	private static final String WEATHER_SERVICE_PACKAGE = "org.omnirom.omnijaws";
+	private static final String CHRONUS_ICON_PACK_INTENT = "com.dvtonder.chronus.ICON_PACK";
+	
     private static final String LOCKSCREEN_TEMP_COLOR = "lock_screen_weather_temp_color";
     private static final String LOCKSCREEN_CONDITION_COLOR = "lock_screen_weather_con_color";
     private static final String PREF_ICON_COLOR = "weather_icon_color";
     private static final String LOCKSCREEN_CITY_COLOR = "lock_screen_weather_city_color";
 	static final int DEFAULT = 0xffffffff;
 
-    private ListPreference mConditionIcon;
+    private PreferenceCategory mWeatherCategory;
+    private ListPreference mWeatherIconPack;
     private ListPreference mHideWeather;
     private SeekBarPreference mNumberOfNotifications;
 	
@@ -88,14 +114,6 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
 
         int intColor;
         String hexColor;
-
-        mConditionIcon =
-                (ListPreference) findPreference(PREF_CONDITION_ICON);
-        int conditionIcon = Settings.System.getInt(mResolver,
-               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
-        mConditionIcon.setValue(String.valueOf(conditionIcon));
-        mConditionIcon.setSummary(mConditionIcon.getEntry());
-        mConditionIcon.setOnPreferenceChangeListener(this);
 
         mHideWeather =
                 (ListPreference) findPreference(PREF_HIDE_WEATHER);
@@ -149,6 +167,7 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
         mIconColor.setOnPreferenceChangeListener(this);
 
         updatePreference();
+		initweathercat();
     }
 
     @Override
@@ -177,16 +196,8 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        boolean value;
 		ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mConditionIcon) {
-            int intValue = Integer.valueOf((String) newValue);
-            int index = mConditionIcon.findIndexOfValue((String) newValue);
-            Settings.System.putInt(mResolver,
-                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
-            mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
-            return true;
-        } else if (preference == mHideWeather) {
+        if (preference == mHideWeather) {
             int intValue = Integer.valueOf((String) newValue);
             int index = mHideWeather.findIndexOfValue((String) newValue);
             Settings.System.putInt(mResolver,
@@ -232,7 +243,106 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
                  Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR, intHex);
          preference.setSummary(hex);
          return true;
+        } else if (preference == mWeatherIconPack) {
+                String value = (String) newValue;
+                Settings.System.putString(getContentResolver(),
+                    Settings.System.OMNIJAWS_WEATHER_ICON_PACK, value);
+                int valueIndex = mWeatherIconPack.findIndexOfValue(value);
+                mWeatherIconPack.setSummary(mWeatherIconPack.getEntries()[valueIndex]);
+               return true;
         }
         return false;
     }
+	
+   public void initweathercat() {
+        mWeatherCategory = (PreferenceCategory) getPreferenceScreen().findPreference(CATEGORY_WEATHER);
+             if (mWeatherCategory != null && !isOmniJawsServiceInstalled()) {
+             getPreferenceScreen().removePreference(mWeatherCategory);
+             } else {
+             String settingsJaws = Settings.System.getString(getContentResolver(),
+                     Settings.System.OMNIJAWS_WEATHER_ICON_PACK);
+             if (settingsJaws == null) {
+                 settingsJaws = DEFAULT_WEATHER_ICON_PACKAGE;
+             }
+             mWeatherIconPack = (ListPreference) findPreference(WEATHER_ICON_PACK);
+ 
+             List<String> entriesJaws = new ArrayList<String>();
+             List<String> valuesJaws = new ArrayList<String>();
+             getAvailableWeatherIconPacks(entriesJaws, valuesJaws);
+             mWeatherIconPack.setEntries(entriesJaws.toArray(new String[entriesJaws.size()]));
+             mWeatherIconPack.setEntryValues(valuesJaws.toArray(new String[valuesJaws.size()]));
+ 
+             int valueJawsIndex = mWeatherIconPack.findIndexOfValue(settingsJaws);
+             if (valueJawsIndex == -1) {
+                 // no longer found
+                 settingsJaws = DEFAULT_WEATHER_ICON_PACKAGE;
+                 Settings.System.putString(getContentResolver(),
+                         Settings.System.OMNIJAWS_WEATHER_ICON_PACK, settingsJaws);
+                 valueJawsIndex = mWeatherIconPack.findIndexOfValue(settingsJaws);
+             }
+             mWeatherIconPack.setValueIndex(valueJawsIndex >= 0 ? valueJawsIndex : 0);
+             mWeatherIconPack.setSummary(mWeatherIconPack.getEntry());
+             mWeatherIconPack.setOnPreferenceChangeListener(this);
+          }
+   }
+
+    private boolean isOmniJawsServiceInstalled() {
+         return PackageUtils.isAvailableApp(WEATHER_SERVICE_PACKAGE, getActivity());
+     }
+ 
+     private void getAvailableWeatherIconPacks(List<String> entries, List<String> values) {
+         Intent i = new Intent();
+         PackageManager packageManager = getPackageManager();
+         i.setAction("org.omnirom.WeatherIconPack");
+         for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+             String packageName = r.activityInfo.packageName;
+             Log.d("maxwen", packageName);
+             if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
+                 values.add(0, r.activityInfo.name);
+             } else {
+                 values.add(r.activityInfo.name);
+             }
+             String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+             if (label == null) {
+                 label = r.activityInfo.packageName;
+             }
+             if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
+                 entries.add(0, label);
+             } else {
+                 entries.add(label);
+             }
+         }
+         i = new Intent(Intent.ACTION_MAIN);
+         i.addCategory(CHRONUS_ICON_PACK_INTENT);
+         for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+             String packageName = r.activityInfo.packageName;
+             values.add(packageName + ".weather");
+             String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+             if (label == null) {
+                 label = r.activityInfo.packageName;
+             }
+             entries.add(label);
+         }
+     }
+ 
+     private boolean isOmniJawsEnabled() {
+         final Uri SETTINGS_URI
+             = Uri.parse("content://org.omnirom.omnijaws.provider/settings");
+ 
+         final String[] SETTINGS_PROJECTION = new String[] {
+             "enabled"
+         };
+ 
+         final Cursor c = getContentResolver().query(SETTINGS_URI, SETTINGS_PROJECTION,
+                 null, null, null);
+         if (c != null) {
+             int count = c.getCount();
+             if (count == 1) {
+                 c.moveToPosition(0);
+                 boolean enabled = c.getInt(0) == 1;
+                 return enabled;
+             }
+         }
+         return true;
+     }
 }
